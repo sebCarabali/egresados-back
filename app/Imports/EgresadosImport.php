266ff85e593270e095b91b6105673ea183c586c2
Sebastián Carabali <sebastiancc@unicauca.edu.co>
2019-11-03 11:19:59 -0500

    Solving merge conflicts

--- app/Imports/EgresadosImport.php
+++ app/Imports/EgresadosImport.php
@@ -3,27 +3,41 @@
 namespace App\Imports;
 
 use Illuminate\Support\Collection;
-use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\{ToCollection};
 
 class EgresadosImport implements ToCollection
 {
 
     public $egresados;
+    public $len;
+
+    public function __construct()
+    {
+        $this->egresados = [];
+        $this->len = 0;
+    }
 
     /**
     * @param Collection $collection
     */
     public function collection(Collection $rows)
     {
-     
-        $this->egresados = array();
         foreach($rows as $row) {
             $egresado = [
                 'nombres' => $row[6],
                 'apellidos' => $row[7],
-                'cedula' => $row[8]
+                'identificacion' => $this->_obtenerIdentificacion($row[8])
+                // TODO: Obtener demas datos del excel.
             ];
             array_push($this->egresados, $egresado);
+            $this->len += 1;
         }
+    }
+
+    private function _obtenerIdentificacion($identificacion)
+    {
+        $retorno = [];
+        preg_match('/[0-9]+(.[0-9])+/', $identificacion, $retorno);
+        return $retorno;
     }
 }

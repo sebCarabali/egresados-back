05bffaba81d1a2d700d99347dd42c13e7e1de7c9
Carlos Andres Chapid Inga <carloschapid@unicauca.edu.co>
2019-11-09 16:02:30 -0500

    Exception comentadas, En desarrollo

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -62,9 +62,11 @@ class Handler extends ExceptionHandler
                 $this->response($exception, 'No records found for: :model',  404, [], ["model" => str_replace("App\\", "", $exception->getModel())]);
             } else if ($exception instanceof MethodNotAllowedHttpException) {
                 $this->response($exception, "The route does not exist!", 404);
-            } else if ($exception instanceof QueryException) {
-                $this->response($exception, "An error occurred while consulting the database.");
-            } else if ($exception instanceof ValidationException) {
+            } 
+            // else if ($exception instanceof QueryException) {
+            //     $this->response($exception, "An error occurred while consulting the database.");
+            // }
+             else if ($exception instanceof ValidationException) {
                 $this->response($exception, "The given data was invalid.", 422, $exception->errors());
             }
             else if($exception instanceof NotFoundHttpException){

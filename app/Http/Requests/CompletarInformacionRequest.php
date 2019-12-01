4f0d8f636189a9b1a0859f0896f552fe4d163056
darwin-ledesma <yamidledesma@unicauca.edu.co>
2019-11-30 20:22:08 -0500

    completar informacion, verificar carnetizacion

--- app/Http/Requests/CompletarInformacionRequest.php
+++ app/Http/Requests/CompletarInformacionRequest.php
@@ -51,7 +51,7 @@ class CompletarInformacionRequest extends FormRequest
 
         ];
     }
-    /*
+    
     public function messages()
     {
         return [
@@ -61,34 +61,34 @@ class CompletarInformacionRequest extends FormRequest
 
             
 
-            'referidos.nombres.*.require'=>'Debe proporcionar un nombre.',
-            'referidos.nombres.*.string'=>'Debe proporcionar un nombre en formato texto.',
-            'referidos.es_egresado.*.require'=>'Debe identificar si es o no un egresado.',
-            'referidos.id_nivel_educativo.*.integer'=>'Debe proporcionar un valor numerico.',
-            'referidos.telefono_movil.*.require'=>'Debe proporcionar un numero telfonico.',
-            'referidos.telefono_movil.*.string'=>'Debe proporcionar un conjunto de numeros de texto.',
-            'referidos.parentesco.*.require'=>'Debe proporcionar un tipo de parentesco.',
-            'referidos.parentesco.*.string'=>'Debe proporcionar un valor de tipo texto.',
-		    'referidos.correo.*.email'=>'Debe proporcionar un correo electronico.',
-            'referidos.id_aut_programa.*.integer'=>'Debe proporcionar un valor numerico.',
+            'referidos.nombres.require'=>'Debe proporcionar un nombre.',
+            'referidos.nombres.string'=>'Debe proporcionar un nombre en formato texto.',
+            'referidos.es_egresado.require'=>'Debe identificar si es o no un egresado.',
+            'referidos.id_nivel_educativo.integer'=>'Debe proporcionar un valor numerico.',
+            'referidos.telefono_movil.require'=>'Debe proporcionar un numero telfonico.',
+            'referidos.telefono_movil.string'=>'Debe proporcionar un conjunto de numeros de texto.',
+            'referidos.parentesco.require'=>'Debe proporcionar un tipo de parentesco.',
+            'referidos.parentesco.string'=>'Debe proporcionar un valor de tipo texto.',
+		    'referidos.correo.email'=>'Debe proporcionar un correo electronico.',
+            'referidos.id_aut_programa.integer'=>'Debe proporcionar un valor numerico.',
 
-            'experiencia.nombre_empresa.*.require'=>'Debe proporcionar un nombre para la empresa.',
-            'experiencia.nombre_empresa.*.string'=>'Debe proporcionar un conjuto de datos de tipo texto.',
-            'experiencia.dir_empresa.*.require'=>'Debe proporcionar una direccion de la empresa.',
-            'experiencia.trabajo_en_su_area.*.require'=>'Debe proporcionar área en la que trabaja.',
-            'experiencia.id_ciudad.*.integer'=>'Debe proporcionar un valor numerico.',
-            'experiencia.id_ciudad.*.require'=>'Debe porporcionar una ciudad.',
-            'experiencia.tel_trabajo.*.require'=>'Debe proporcionar un numero de teléfono.',
-            'experiencia.tel_trabajo.*.string'=>'Debe proporcionar un conjunto de numeros de texto.',
+            'experiencia.nombre_empresa.require'=>'Debe proporcionar un nombre para la empresa.',
+            'experiencia.nombre_empresa.string'=>'Debe proporcionar un conjuto de datos de tipo texto.',
+            'experiencia.dir_empresa.require'=>'Debe proporcionar una direccion de la empresa.',
+            'experiencia.trabajo_en_su_area.require'=>'Debe proporcionar área en la que trabaja.',
+            'experiencia.id_ciudad.integer'=>'Debe proporcionar un valor numerico.',
+            'experiencia.id_ciudad.require'=>'Debe porporcionar una ciudad.',
+            'experiencia.tel_trabajo.require'=>'Debe proporcionar un numero de teléfono.',
+            'experiencia.tel_trabajo.string'=>'Debe proporcionar un conjunto de numeros de texto.',
 
-            'experiencia.rango_salario.*.require'=>'Debe proporcionar un rango salarial.',
-            'experiencia.rango_salario.*.string'=>'Debe proporcionar un conjuto de datos tipo texto.',
-            'experiencia.tipo_contrato.*.require'=>'Debe proporcinar un tipo de contrato de forma correcta.',
-            'experiencia.sector.*.string'=>'Debe proporcionar un conjunto de datos tipo texto.',
-            'experiencia.cargo_nombre.*.require'=>'Debe proporcionar un tipo de cargo valido',
-            'experiencia.cargo_nombre.*.string'=>'Debe proporcionar un conjuto de datos tipo texto.',
-            'experiencia.estado_experiencia.*.require'=>'Debe proporcionar un estado de experiencia correcta.'
+            'experiencia.rango_salario.require'=>'Debe proporcionar un rango salarial.',
+            'experiencia.rango_salario.string'=>'Debe proporcionar un conjuto de datos tipo texto.',
+            'experiencia.tipo_contrato.require'=>'Debe proporcinar un tipo de contrato de forma correcta.',
+            'experiencia.sector.string'=>'Debe proporcionar un conjunto de datos tipo texto.',
+            'experiencia.cargo_nombre.require'=>'Debe proporcionar un tipo de cargo valido',
+            'experiencia.cargo_nombre.string'=>'Debe proporcionar un conjuto de datos tipo texto.',
+            'experiencia.estado_experiencia.require'=>'Debe proporcionar un estado de experiencia correcta.'
             
         ];
-    }*/
+    }
 }

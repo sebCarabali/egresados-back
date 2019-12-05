2969f286be98dc681c0aa7d4528703baff4989b5
sebastian carabali <sebastiancc@unicauca.edu.co>
2019-12-05 13:12:25 -0500

    Se completa la gestión de apoyos

--- app/Http/Controllers/ApoyoController.php
+++ app/Http/Controllers/ApoyoController.php
@@ -7,13 +7,25 @@ use App\Helpers\CrearUsuario;
 use App\Http\Resources\ApoyoResource;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\DB;
 
 class ApoyoController extends Controller
 {
     public function getAll(Request $request)
     {
-        return ApoyoResource::collection(Apoyo::all());
+        $apoyos = Apoyo::all();
+        $page = $request->get('page');
+        $pageSize = $request->get('page_size');
+        $results = $apoyos->slice(($page - 1) * $pageSize, $pageSize)->values();
+        return ApoyoResource::collection(
+            new LengthAwarePaginator(
+                $results,
+                $total = count($apoyos),
+                $pageSize,
+                $page
+            )
+        );
     }
 
     public function getById($idApoyo)
@@ -27,13 +39,13 @@ class ApoyoController extends Controller
 
     public function save(Request $request)
     {
-        $data = $request->only('nombres', 'apellidos', 'correo', 'correoSecundario', 'servicios');
+        $data = $request->only('nombres', 'nombreRol', 'apellidos', 'correo', 'correoSecundario', 'servicios');
         DB::beginTransaction();
         try {
             $apoyo = new Apoyo([
                 'nombres' => $data['nombres'],
                 'apellidos' => $data['apellidos'],
-                'nombre_rol' => $data['nombreRol'],
+                'nombre_rol' => $request->get('nombreRol'),
                 'correo' => $data['correo'],
                 'correo_secundario' => $data['correoSecundario']
             ]);
@@ -54,17 +66,20 @@ class ApoyoController extends Controller
 
     public function update(Request $request)
     {
-        $data = $request->only('id','nombres', 'apellidos', 'nombreRol', 'correo', 'correoSecundario', 'servicios');
+        $data = $request->only('id', 'nombres', 'apellidos', 'nombreRol', 'correo', 'correoSecundario', 'servicios', 'usuario');
         DB::beginTransaction();
         try {
             $apoyo = Apoyo::where('id_aut_apoyo', $data['id'])->first();
-            if($apoyo) {
+            if ($apoyo) {
                 $apoyo->nombres = $data['nombres'];
                 $apoyo->apellidos = $data['apellidos'];
                 $apoyo->nombre_rol = $data['nombreRol'];
                 $apoyo->correo_secundario = $data['correoSecundario'];
+                $usuario = $apoyo->usuario()->first();
+                $usuario->activo = $data['usuario']['activo'];
+                $usuario->save();
                 $idServicios = [];
-                foreach($data['servicios'] as $serv) {
+                foreach ($data['servicios'] as $serv) {
                     array_push($idServicios, $serv['id']);
                 }
                 $apoyo->servicios()->sync($idServicios);
@@ -72,7 +87,7 @@ class ApoyoController extends Controller
                 DB::commit();
                 return response()->json(['data' => new ApoyoResource($apoyo)], 201);
             }
-        } catch (Exception $e) { 
+        } catch (Exception $e) {
             DB::rollback();
             return response()->json(['error' => 'Error actualizando datos del apoyo: ' . $e->getMessage()], 400);
         }

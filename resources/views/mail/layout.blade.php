7a27ea069c91f73e6e8794228f43153729147fa2
Carlos Andres Chapid Inga <carloschapid@unicauca.edu.co>
2019-12-17 11:42:15 -0500

    Cambio validacion contraseña empresa

--- resources/views/mail/layout.blade.php
+++ resources/views/mail/layout.blade.php
@@ -16,64 +16,66 @@
                               <div class="gmail_quote">
                                     <div dir="ltr">
                                           <center>
-                                                <img src="https://raw.githubusercontent.com/Fher95/OfertasEgresadosFront/desarrollo/src/assets/img/egresados-logo.png" width="100%" style="max-width: 400px; alt="">
+                                                <img src="https://raw.githubusercontent.com/Fher95/OfertasEgresadosFront/desarrollo/src/assets/img/egresados-logo.png"
+                                                      width="100%" style="max-width: 400px; alt="">
                                           </center>
                                           <div><br>
-                                                <div style="font-size:small">
-                                                      <font face="arial black,
+                                                <div style=" font-size:small">
+                                                <font face="arial black,
                       sans-serif"><br>
-                                                      </font>
-                                                </div>
-                                                <div>
-                                                      <div style="text-align:center">
-                                                            <font size="4" face="georgia, serif" color="#cc0000">
-                                                                  <i><b>@yield("titulo_mensaje")</b></i></font>
-                                                      </div>
-                                                      <div style="font-size:small;text-align:center">
-                                                            @yield('contenido')
-                                                      </div>
-                                                      <br>
-                                                </div>
+                                                </font>
+                                    </div>
+                                    <div>
+                                          <div style="text-align:center">
+                                                <font size="4" face="georgia, serif" color="#cc0000">
+                                                      <i><b>@yield("titulo_mensaje")</b></i></font>
+                                          </div>
+                                          <div style="font-size:small;text-align:center">
+                                                @yield('contenido')
+                                          </div>
+                                          <br>
+                                    </div>
 
-                                                <div>
-                                                      <div style="text-align:center">
-                                                            <font size="4" color="#cc0000"><b>Este servicio es
-                                                                        totalmente GRATUITO</b></font>
-                                                      </div>
-                                                </div>
-
+                                    <div>
+                                          <div style="text-align:center">
+                                                <font size="4" color="#cc0000"> <b>Este servicio es totalmente
+                                                            GRATUITO</b>
+                                                </font>
                                           </div>
                                     </div>
+
                               </div>
                         </div>
                   </div>
             </div>
-            <hr style="height: 3px; background-color: gray; border: 0px;">
-            <footer>
-                  <small>
+      </div>
+      </div>
+      <hr style="height: 3px; background-color: gray; border: 0px;">
+      <footer>
+            <small>
 
-                        <div class="container">
-                              <a> CONTACTO ÁREA DE EGRESADOS </a>
-                              <ul class="list-inline col-5">
-                                    <li> <a> Dirección: Calle 5 N° 4-70</a>
-                                    </li>
-                                    <li> <a> Centro Histórico - Santo Domingo - Popayán</a>
-                                    </li>
-                                    <li> <a> Teléfono: 8209900 Ext. 1431</a>
-                                    </li>
-                                    <li> <a> Correo electrónico: egresados@unicauca.edu.co</a>
-                                    </li>
-                              </ul>
-                        </div>
-                  </small>
-                  <br>
-                  <br>
-                  <div style="text-align: center;"><small>
-                              © 2019 Copyright:
-                              <a href="http://www.unicauca.edu.co/versionP/">Universidad Del Cauca</a>
-                        </small>
+                  <div class="container">
+                        <a> CONTACTO ÁREA DE EGRESADOS </a>
+                        <ul class="list-inline col-5">
+                              <li> <a> Dirección: Calle 5 N° 4-70</a>
+                              </li>
+                              <li> <a> Centro Histórico - Santo Domingo - Popayán</a>
+                              </li>
+                              <li> <a> Teléfono: 8209900 Ext. 1431</a>
+                              </li>
+                              <li> <a> Correo electrónico: egresados@unicauca.edu.co</a>
+                              </li>
+                        </ul>
                   </div>
-            </footer>
+            </small>
+            <br>
+            <br>
+            <div style="text-align: center;"><small>
+                        © 2019 Copyright:
+                        <a href="http://www.unicauca.edu.co/versionP/">Universidad Del Cauca</a>
+                  </small>
+            </div>
+      </footer>
       </div>
 </body>
 

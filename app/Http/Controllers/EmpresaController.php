51ad30ae8761bd133b1c20a7a4d66ebd8441e8c6
Carlos Andres Chapid Inga <carloschapid@unicauca.edu.co>
2019-12-17 23:26:58 -0500

    Obtenre empresa por correo

--- app/Http/Controllers/EmpresaController.php
+++ app/Http/Controllers/EmpresaController.php
@@ -429,8 +429,10 @@ class EmpresaController extends Controller
             if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
                 $user = User::whereEmail($email)->get()->first();
                 if($user){
-                    // $user = new User();
-                    return $this->success($user->administrador()->with("empresa")->get());
+                    $empresa = new Empresa();
+                    $empresa->id_aut_empresa= $user->administrador->empresa->id_aut_empresa;
+                    
+                    return $this->success($empresa);
                 }else{
                     return $this->fail("No se encontro nignun usuario!", 422);
                 }

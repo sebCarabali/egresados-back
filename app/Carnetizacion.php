4c048f0540f989627d63153dc2d035f2ded5081f
sebastian carabali <sebastiancc@unicauca.edu.co>
2020-02-03 08:24:12 -0500

    Se cambia la llave primaria de Carnetización

--- app/Carnetizacion.php
+++ app/Carnetizacion.php
@@ -7,12 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Carnetizacion extends Model
 {
     protected $table = 'carnetizacion';
-    protected $primaryKey = 'id_carnetizacion';
+    protected $primaryKey = 'id_aut_carnetizacion';
     protected $fillable = ['nombres'];
     public $timestamps = false;
 
 
-    public function egresados() {
-        return $this->belongsToMany('App\Egresado', 'solicita', 'id_carnetizacion','id_egresados');
+    public function egresados()
+    {
+        return $this->belongsToMany('App\Egresado', 'solicita', 'id_carnetizacion', 'id_egresados');
     }
 }

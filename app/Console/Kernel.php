58189348f4e6fc64dad442340dfbeccd04ab146f
Camilo Forero <juancamiforero@unicauca.edu.co>
2020-02-24 10:21:54 -0500

    Commit para pull

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        'App\Console\Commands\VencimientoOferta',
     ];
 
     /**
@@ -26,6 +26,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->command('vencimiento:ofertas')->daily();
+        //$schedule->command('vencimiento:ofertas')->everyMinute();
     }
 
     /**
